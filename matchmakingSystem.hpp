6fc95bbc143aa7bb44f1fc73c9137e7ca04f7f57
Luis Felipe Veloso Canha <C3007818@fgv.edu.br>
2026-05-05 09:49:40 -0300

    commitando p testar no codespaces

--- matchmakingSystem.hpp
+++ matchmakingSystem.hpp
@@ -1,1 +1,51 @@
- log
+#ifndef MATCHMAKING_HPP
+#define MATCHMAKING_HPP
+
+#include <iostream>
+
+class Player {
+private:
+    int id;
+    std::string name;
+    int score;
+    int timestamp;
+
+public:
+
+    Player() = default;
+    Player(int id, std::string name, int score, int timestamp);
+    ~Player() = default;
+
+    int getId();
+    std::string getName();
+    int getScore();
+    int getTimestamp();
+};
+
+class Matchmaking {
+private:
+
+    Player players[MAX_PLAYERS];
+    int size;
+
+public:
+
+    Matchmaking() = default;
+    ~Matchmaking() = default;
+
+    bool insert(Player player);
+    bool removePlayer(int id);
+
+    void sortByScoreInsertion();
+    void sortByScoreMerge();
+
+    Player* formGroup(int groupSize, int delta, int* n);
+
+    Player* getWaitingPlayers(int* n);
+
+    void printWaitingPlayers();
+
+    // Outros métodos auxiliares, se necessário
+};
+
+#endif
