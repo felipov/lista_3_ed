75e6481490366024c8d0de5a08fba42bd7585a9d
ElianMiguel <elianferreira266@gmail.com>
2026-05-14 20:16:34 -0300

    criação da main.cpp (incompleto)

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include "Player.hpp"
+#include "Matchmaking.hpp"
+
+int main() {
+    // Inicialização do sistema e dos jogadores
+    Matchmaking system();
+    Player p1(1, "João", 980, 1);
+    Player p2(2, "Maria", 1080, 2);
+    Player p3(3, "Maurício", 1110, 3);
+    Player p4(4, "Joana", 1110, 4);
+    Player p5(5, "Túlio", 820, 5);
+    Player p6(6, "Arnaldo", 1110, 6);
+
+    // Funcionamento do sistema
+    
+}
